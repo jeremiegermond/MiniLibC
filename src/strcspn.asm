aeaa4bd4185fc37e213aeca36d5ec0c7f7b98cdd
Jeremie <jeremie@localhost.localdomain>
2022-02-24 21:27:51 +0100

    fix: strcspn

--- src/strcspn.asm
+++ src/strcspn.asm
@@ -3,28 +3,33 @@ global strcspn
 section .text
 
 strcspn:
+	xor rax, rax
 	xor rcx, rcx
-	xor	r8, r8
-    xor r9, r9
 
 loop:
-	mov	al, byte[rdi + rcx]
-	cmp	al, 0
+	mov al, byte[rdi + rcx]
+	cmp al, 0
 	je end
-	inc	rcx
+	inc rcx
 
-substractloop:
-	mov	r8b, byte[rsi + r9]
-	cmp	r8b, 0
-	je	loop
-	cmp	al, r8b
-	je	subindex
-	inc	r9
-	jmp	substractloop
+search:
+	xor rbx, rbx
 
-subindex:
-	sub rcx, 1
+next_char:
+	cmp byte[rsi + rbx], 0
+	je loop
+	cmp al, byte[rsi + rbx]
+	je found
+	inc rbx
+	jmp next_char
+
+found:
+	dec rcx
 	mov rax, rcx
+	ret
+
+endnull:
+	mov rax, 0
 	ret
 
 end:
