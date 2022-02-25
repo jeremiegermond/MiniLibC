62f145d0b4e4e1db66e2d98b7771ced2416e5b6f
Jeremie <jeremie@localhost.localdomain>
2022-02-25 14:57:35 +0100

    fix: memmove

--- src/memmove.asm
+++ src/memmove.asm
@@ -3,22 +3,38 @@ global memmove
 section .text
 
 memmove:
-    xor rcx, rcx
+    cmp rdi, rsi
+    jg right
+    jl left
+    jmp end
 
-loopleft:
-    cmp rcx, rdx
-    je end
-    mov r8b, byte[rsi + rcx]
-    mov byte[rdi + rcx], r8b
-    jmp loopleft
-
-loopright:
-    cmp rdx, 0
-    jb end
-    mov r8b, byte[rsi + rdx]
-    mov byte[rdi + rdx], r8b
-    dec rdx
+right:
+    mov r8, 0
     jmp loopright
 
+loopright:
+    cmp r8, rdx
+    jae end
+    mov al, byte[rsi + r8]
+    mov byte[rdi + r8], al
+    inc r8
+    jmp loopright
+
+left:
+    mov r8, 0
+    jmp loopleft
+
+loopleft:
+    cmp r8, rdx
+    jae end
+    mov r9, rdx
+    sub r9, r8
+    dec r9
+    mov al, byte[rsi + r9]
+    mov byte[rdi + r9], al
+    inc r8
+    jmp loopleft
+
 end:
+    mov rax, rdi
     ret
